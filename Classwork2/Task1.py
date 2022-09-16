51ac3f830976615d7c9dfa1832c09c95e43f4eb6
Midyadi <midyadi20041106@gmail.com>
2022-09-16 10:07:27 +0300

    Commit2

--- Classwork2/Task1.py
+++ Classwork2/Task1.py
@@ -1,5 +1,5 @@
 import itertools as it
-with open("input.txt", 'rt',encoding='utf-8') as i, open('output.txt', 'w', encoding = 'utf-8') as o:
+with open("input.txt",'rt', encoding = 'utf-8') as i, open('output.txt', 'w',encoding = 'utf-8') as o:
     a = [[int(i) for i in line.strip().split()] for line in i.readlines()]
     b = map(lambda x: sum(x), it.zip_longest(*a))
     print(*b, file = o)
