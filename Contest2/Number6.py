06a4cfba73c13031da04c3078143123c9fd99367
Midyadi <midyadi20041106@gmail.com>
2022-09-16 11:44:57 +0300

    Commit3

---
+++ Contest2/Number6.py
@@ -0,0 +1,8 @@
+with open('input.txt', 'r', encoding='utf-8') as i:
+    max_grades = [0,0,0]
+    pupils = [pupil.strip().split() for pupil in i.readlines()]
+    for pupil in pupils:
+        grade,mark = [int(i) for i in pupil[-2:]]
+        if mark>max_grades[grade-9]:
+            max_grades[grade - 9] = mark
+    print(*max_grades)
