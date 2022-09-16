0e1b3f69cf4f1bea4afc2d2c15f0aed433af6c72
Midyadi <midyadi20041106@gmail.com>
2022-09-16 11:57:54 +0300

    Commit3

---
+++ Contest2/Number8.py
@@ -0,0 +1,4 @@
+with open('input.txt', 'r', encoding='utf-8') as i:
+    pupils = sorted([pupil.strip().split() for pupil in i.readlines()], key = lambda x: x[0])
+    for pupil in pupils:
+        print(*pupil)
