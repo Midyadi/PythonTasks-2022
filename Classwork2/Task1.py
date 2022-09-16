72a85c1a44313b4d096454eeb3bc87dc7b3acdce
Midyadi <midyadi20041106@gmail.com>
2022-09-16 10:28:47 +0300

    Commit3

--- Classwork2/Task1.py
+++ Classwork2/Task1.py
@@ -1,6 +1,6 @@
 import itertools as it
 with open("input.txt",'rt', encoding = 'utf-8') as i, open('output.txt', 'w',encoding = 'utf-8') as o:
-    a = [[int(i) for i in line.strip().split()] for line in i.readlines()]
-    b = map(lambda x: sum(x), it.zip_longest(*a))
-    print(*b, file = o)
+    matrix = [[int(num) for num in line.strip().split()] for line in i.readlines()]
+    summa = map(lambda x: sum(x), it.zip_longest(*matrix, fillvalue = 0))
+    print(*summa, file = o)
 
--- Classwork2/Task2.py
+++ Classwork2/Task2.py
@@ -1,5 +1,5 @@
 import itertools as iter
 with open('input.txt', 'rt', encoding = 'utf-8') as i, open('output.txt', 'w', encoding='utf-8') as o:
     count,*lines = i.readlines()
-    for i,l in zip(iter.cycle(range(int(count))), lines):
-        print(i, l, file = o, end = '')
+    for number,line in zip(iter.cycle(range(int(count))), lines):
+        print(number,line, file = o, end = '')
