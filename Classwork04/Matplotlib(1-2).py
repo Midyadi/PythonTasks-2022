25d1a8b99a7a7af505babcac094b4d1563c3d2e3
Midyadi <midyadi20041106@gmail.com>
2022-09-28 10:58:45 +0300

    Matrix and matplotlib(1-2)

---
+++ Classwork04/Matplotlib(1-2).py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+mpl.rcParams['font.size']=16
+plt.figure(figsize=(7,7))
+plt.title(r'$y = \sin x \cdot x^{-2}$')
+plt.xlabel("Ось X")
+plt.ylabel("Ось Y")
+x_data = np.linspace(0,100,11)
+y_data = np.sin(x_data) * pow(x_data,-2*(x_data>0))
+plt.plot(x_data,y_data,'r^', label = "Красивый график")
+
+plt.grid(b=True, which='major', axis='both', alpha=1)
+plt.grid(b=True, which='minor', axis='both', alpha=0.5)
+
+plt.savefig('Graph Matplotlib(1-2).jpg')
