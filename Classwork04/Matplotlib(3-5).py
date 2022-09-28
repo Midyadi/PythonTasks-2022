d853435c00c9a7d9d83b0ae656f77f7d788bf559
Midyadi <midyadi20041106@gmail.com>
2022-09-28 11:26:56 +0300

    matplotlib(1-2) fix and matplotlib(3-5)

---
+++ Classwork04/Matplotlib(3-5).py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rcParams['font.size']=16
+plt.figure(figsize=(7,7))
+
+plt.title(r'$e ^{-x \cdot \sin x}$')
+plt.xlabel("Ось X")
+plt.ylabel("Ось Y")
+
+x_data = np.linspace(10,100,1001)
+y_data = np.exp(-x_data * np.sin(x_data))
+
+plt.plot(x_data, y_data, label="Ещё один красивый график")
+
+plt.grid(b=True, which='major', axis='both', alpha=1)
+plt.grid(b=True, which='minor', axis='both', alpha=0.5)
+plt.minorticks_on()
+plt.ylim(0,5)
+
+
+plt.savefig('Graph Matplotlib(3-5).jpg')
